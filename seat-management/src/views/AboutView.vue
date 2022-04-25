46b2f7b9cf3b533f67ad8088644484dba015ea14
STEVEN\Adminsss <loctrinhct@gmail.com>
2022-04-25 20:36:52 +0700

    investigate translate seat position

--- seat-management/src/views/AboutView.vue
+++ seat-management/src/views/AboutView.vue
@@ -1,5 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="background">
+      <strong
+        class="test-button"
+        :style="{ left: leftValue, top: topValue }"
+        :key="left"
+      >
+        o
+      </strong>
+    </div>
   </div>
 </template>
+
+<script>
+import { result } from "../test-convert/convert-svg.ts";
+export default {
+  async created() {
+    await this.checkConvert();
+  },
+  data() {
+    return {
+      leftValue: "0px",
+      topValue: "0px",
+    };
+  },
+  // computed: {
+  //   leftValue() {
+  //     return this.left;
+  //   },
+  //   topValue() {
+  //     return this.top;
+  //   },
+  // },
+  methods: {
+    async checkConvert() {
+      const seatMap = result.svg.g.path;
+      await this.findSeat(seatMap);
+    },
+    async findSeat(seatMap) {
+      // seatMap.map(ele=>{
+
+      // })
+      const scaleX = 2463 / 1080;
+      const scaleY = 1212 / 530;
+      const object = seatMap[101]._attributes.d;
+      const MValue = object
+        .slice(object.indexOf("M") + 2, object.indexOf("L") - 1)
+        .split(" ");
+      console.log(scaleX, scaleY, MValue[0] / scaleX, MValue[1] / scaleY);
+      this.leftValue = MValue[0] / scaleX + "px";
+      this.topValue = MValue[1] / scaleX + 78.4 + 8 + "px";
+    },
+  },
+};
+</script>
+<style scoped lang="css">
+.container {
+  display: absolute;
+}
+.background {
+  background-size: 1080px;
+  height: 530px;
+  width: 1080px;
+  background-image: url("../svg/example.drawio.svg");
+}
+.test-button {
+  position: absolute;
+  color: red;
+}
+</style>
