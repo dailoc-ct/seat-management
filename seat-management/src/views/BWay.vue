920865795a2dc11a34e2a6506742e34a6f2cb36b
STEVEN\Adminsss <loctrinhct@gmail.com>
2022-04-26 15:21:11 +0700

    render seat using target table

---
+++ seat-management/src/views/BWay.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <div class="background">
+      <template v-if="checkSeats">
+        <button
+          v-for="seat in seats"
+          :key="seat.x"
+          :style="{ left: seat.x, top: seat.y }"
+          class="test-button"
+        >
+          A
+        </button>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import { result } from "../test-convert/convert-svg.ts";
+export default {
+  data() {
+    return {
+      seats: [],
+      leftValue: "0px",
+      topValue: "0px",
+    };
+  },
+  async mounted() {
+    await this.checkConvert();
+  },
+  computed: {
+    checkSeats() {
+      return this.seats && this.seats.length > 0;
+    },
+  },
+  methods: {
+    async checkConvert() {
+      const seatMap = result.svg[0].g[0].rect;
+      await this.findSeat(seatMap);
+    },
+    async findSeat(seatMap) {
+      const scaleX = 1642 / 1205;
+      const scaleY = 1032 / 762;
+      seatMap.map((seat) => {
+        const object = seat._attributes;
+        if (object.height === 80 && object.width === 80) {
+          const positionValue = {
+            x: object.x / scaleX + 10 + "px",
+            y: object.y / scaleY + 78.4 + 8 + "px",
+          };
+          this.seats.push(positionValue);
+        }
+      });
+    },
+  },
+};
+</script>
+<style scoped lang="css">
+.container {
+  display: absolute;
+}
+.background {
+  background-size: 100vh;
+  height: 1205px;
+  width: 762px;
+  background-image: url("../svg/floor.svg");
+}
+.test-button {
+  position: absolute;
+  color: red;
+}
+</style>
