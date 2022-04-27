78a3061aaa2ad6b994cd00dd3f71d4b8c913f0b4
STEVEN\Adminsss <loctrinhct@gmail.com>
2022-04-27 18:55:08 +0700

    render seat from db

---
+++ seat-management/src/components/SeatMap.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <button
+      class="seat"
+      v-for="seat in seats"
+      :key="seat"
+      :style="{ left: `${seat.x}px`, top: `${seat.y}px` }"
+    >
+      {{ seat.seatNumber }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, computed, onUpdated } from "vue";
+import { Seat } from "../../public/interface/seatMap.state";
+export default {
+  name: "SeatMap",
+  props: ["seatMap"],
+  setup(props: any) {
+    const seats = computed(() => {
+      return props.seatMap;
+    });
+    return { seats };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  border: solid 2px #000;
+  height: 100vh;
+  display: absolute;
+}
+.seat {
+  min-width: 50px;
+  min-height: 50px;
+  background: red;
+  position: absolute;
+}
+</style>
---
+++ seat-management/src/views/GetData.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <SeatMap :v-if="showMap" :seat-map="seats" />
+  </div>
+</template>
+
+<script lang="ts">
+// import { Options, Vue } from "vue-class-component";
+import SeatMap from "@/components/SeatMap.vue"; // @ is an alias to /src
+import { seatMap } from "../store/index";
+import { onBeforeMount, computed } from "vue";
+import { Seat } from "../../public/interface/seatMap.state";
+
+// @Options({
+//   components: {
+//     SeatMap,
+//   },
+// })
+export default {
+  components: {
+    SeatMap,
+  },
+  setup() {
+    const seatsMap = seatMap();
+    const seats: Seat[] = [];
+    onBeforeMount(() => {
+      seatsMap.fetch();
+      if (seatsMap.seats)
+        seatsMap.seats.map(async (seat) => {
+          seats.push({ ...seat, y: seat.y + 286.4 });
+        });
+    });
+    const showMap = computed(() => {
+      if (seatsMap.seats) {
+        return seats.length === seatsMap.seats?.length;
+      }
+      return false;
+    });
+    return { seats, showMap };
+  },
+};
+</script>
