ab6ab8084b58d5c67a6a1d0c1c6e72766d1f51de
STEVEN\Adminsss <loctrinhct@gmail.com>
2022-04-27 18:57:18 +0700

    import Konva

--- seat-management/src/main.ts
+++ seat-management/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import VueKonva from "vue-konva";
 
-createApp(App).use(router).use(createPinia()).mount("#app");
+createApp(App).use(router).use(createPinia()).use(VueKonva).mount("#app");
